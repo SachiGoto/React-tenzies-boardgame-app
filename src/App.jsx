a55808b3aed85dfdaff6a52c93923cad066c82d9
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2023-01-13 01:53:33 -0800

    added some more features

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
+
+import Confetti from 'react-confetti'
 import { useState } from 'react'
+import {useEffect} from 'react'
 import './App.css'
 import Die from './components/Die'
 import { nanoid } from 'nanoid'
 
+
+
 function App() {
   const [nums, setNums] = useState(allNewDice())
+  const [tenzies, setTenzies] = useState(false)
+  
+
+  useEffect(()=>{
+
+ 
+
+           nums.every(die => die.isHeld === true && die.value === nums[0].value)? setTenzies(true): ''
+
+           
+         
+          
+            
+             
+   
+      //  console.log("Dice state changed")
+          
+  }, [nums])
+
+
+
+
   const value = 0;
  
 
@@ -24,14 +51,26 @@ function App() {
     return newSetOfArray
   }
 
-  console.log(allNewDice())
+  // console.log(allNewDice())
 
   function holdDice(id){
     
-    console.log(id)
+    setNums(prevNums=>{
+       return prevNums.map(die=>{
+            return id === die.id? {
+                   ...die,
+                   isHeld: !die.isHeld
+            }: die
+
+       
+       })
+      
+    })
+   
   }
 
-
+  // console.log(nums)
+  
  let numsArray = nums.map((num)=>{
  
   return(
@@ -45,35 +84,59 @@ function App() {
 
     
   function rollDice(){
-     
-       setNums(prevNumArray=>{
-        return (allNewDice().map(die=>{
+    if(tenzies === true){
+      setNums(allNewDice())
+      setTenzies(false)
+ 
+
+
+
+    }else{
+      setNums(prevNumArray=>{
+        return (allNewDice().map((die, index)=>{
 
            return(
-                {      ...prevNumArray,
+
+            prevNumArray[index].isHeld === false?
+                {      ...prevNumArray[index],
                       "value" : die.value,
-                      "isHeld": true
-                }
+                     
+                }: prevNumArray[index]
            )
         })
         )
        
        })
+
+
+
+
+    }
+      
+
+
+     
+       
   }
 
 
-
+const styles ={ 
+     backgroundColor : tenzies && "Green"
+ }
   
 
   // console.log(nums)
  
 
   return (
+
     <main className="App">
+
+   {tenzies && <Confetti />}
 
  <div className="bigContainer">
  <h1>Tenzies!</h1>
-      <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls. </p>
+      <p className="description">Roll until all dice are the same. Click each die to freeze it at its current value between rolls. </p>
 
  <div className = "die-container">
 
@@ -84,7 +147,7 @@ function App() {
      
   </div>
 
-  <button className="btn" onClick={rollDice}>Roll</button>
+  <button className="btn" onClick={rollDice} style={styles} >{tenzies?"New Game":"Roll"}</button>
 
 
  </div>
